c7c3ff5339c32bfb17d5c40a648f70bd3b3253d4
mugil132004 <mugilmurugan272@gmail.com>
2026-02-25 16:45:55 +0530

    file handling

---
+++ day51/Mugil.java
@@ -0,0 +1,15 @@
+
+package classwork;
+
+
+public class Mugil {
+   
+   public void run()
+   {
+       System.out.println("enter the length");
+       System.out.println("enter the name");
+   }
+    
+    
+    
+}
