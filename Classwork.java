287d9f7167ad44964862ae4cb8b6d10f79759ea2
mugil132004 <mugilmurugan272@gmail.com>
2026-02-25 17:04:47 +0530

    file Handling

---
+++ Classwork.java
@@ -0,0 +1,19 @@
+
+package classwork;
+import java.util.Scanner;
+
+public class Classwork {
+
+    public static void main(String[] args) 
+    {
+        Scanner Sc =new Scanner(System.in);
+        int arr[]=new int[5];
+        System.out.println("enter the length");
+         System.out.println("enter the ");
+        
+        
+        
+       
+    }
+    
+}
